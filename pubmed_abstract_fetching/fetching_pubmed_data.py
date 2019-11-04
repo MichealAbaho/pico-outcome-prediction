282fd061cbb2a2801fc11d53cc60bc3769139abe
Micheal Abaho <michealabaho265@gmail.com>
2019-11-04 14:00:49 +0000

    full pipeline fetching nabstracts from pubmed

--- pubmed_abstract_fetching/fetching_pubmed_data.py
+++ pubmed_abstract_fetching/fetching_pubmed_data.py
@@ -13,6 +13,7 @@ import json
 import os
 import time
 from urllib.error import HTTPError
+from shutil import make_archive
 
 class fetch_data:
     def __init__(self, query_term, key_search_wrds={}, key_fetch_terms={}):
@@ -32,29 +33,30 @@ class fetch_data:
         record = Entrez.read(handle)
         db_precendence = dict()
         for i,j in enumerate(record['eGQueryResult']):
-            db_precendence[j['DbName']] = int(j['Count'])
-            
+            try:
+                db_precendence[j['DbName']] = int(j['Count'])
+            except Exception as e:
+                print(e)
+
         db_precendence = dict(sorted(db_precendence.items(), key=lambda x: x[1], reverse=True)[:5])
         return db_precendence
         
     def fetch_data(self, db_dict):
         attempts = 1
+        file_name = 'all_abstracts_pubdate'
         for k,v in db_dict.items():
-            
             if k == 'pubmed':
                 handle = Entrez.esearch(db=k, **self.search_terms)
                 search_result = Entrez.read(handle)
                 relevant_id_list = search_result['IdList']
                 if relevant_id_list:
-                    
                     try:
                         fetch_query = Entrez.efetch(db=k, id=relevant_id_list, webenv=search_result['WebEnv'], query_key=search_result['QueryKey'], **self.fetch_terms)
                         fetch_result = Medline.parse(fetch_query)
-                        with open('all_abstracts_pubdate', 'w') as abs_pbmd:
+                        with open(file_name, 'w') as abs_pbmd:
                             json.dump(list(fetch_result), abs_pbmd, indent=2, sort_keys=True)
-                           
+                        abs_pbmd.close()
                                 #json.dump(record, abs_pbmd, indent=2)
-                                
                     except HTTPError as e:
                         if 500 <= e.code <= 599:
                             print("Received error from server %s"%e)
@@ -63,34 +65,43 @@ class fetch_data:
                             time.sleep(15)
                         else:
                             raise
-    
-def sort_retrieved_abstracts(json_file, json_file_2):
-    main_dir = '..\\pico-outcome-prediction'
-    abs_dirs = os.path.abspath(os.path.join(main_dir, 'rct_abstracts', 'relevance'))
-    
-    if not os.path.exists(abs_dirs):
-        os.makedirs(abs_dirs)
-    
+        return file_name
+
+def sort_retrieved_abstracts(json_file):
     d,n = [],[]
-    with open(json_file, 'r') as tg_json:
-        ab_bs = json.load(tg_json)
-        for entry in ab_bs:
-            d.append(entry['PMID'])
-            
-    with open(json_file, 'r') as tg_json:
-        ab_bs = json.load(tg_json)
-        for entry in ab_bs:
-            n.append(entry['PMID'])
-    
-    x = [i for i in d if i not in n]
-    print(x)
+    dir_ = os.path.join(os.path.abspath(os.path.curdir), 'new_batch')
+    if not os.path.exists(dir_):
+        os.makedirs(dir_)
+
+    current_dir_files = os.listdir('.')
+    if json_file in current_dir_files:
+        with open(json_file, 'r') as tg_json:
+            ab_bs = json.load(tg_json)
+            for entry in ab_bs:
+                file = open(os.path.join(dir_, entry['PMID']+'_PD.txt'), 'w')
+                file.writelines('PMID: {}\nTitle: {}\nPublication Type: {}\nJournal-Name: {}\nJournal ID: {}\nPublication date: {}\n\n{}'.format(
+                                entry['PMID'],
+                                entry['TI'],
+                                ' '.join([i+',' for i in entry['PT']]),
+                                entry['JT'],
+                                entry['JID'],
+                                entry["CRDT"],
+                                entry['AB']
+
+                ))
+                file.close()
+            tg_json.close()
+
+    #send folder to archive
+    make_archive('extra_abstracts', 'zip', dir_)
+
 
             
             
 if __name__ == "__main__":
     query = 'randomized controlled trial [FILT]' #[FILT] limits the search inpubmed to
     key_wrds_search = {'term':query,
-                       'retmax':300,
+                       'retmax':20,
                        'retmode':'xml',
                        'usehistory':'y',
                        'sort':'pub+date'}
@@ -99,7 +110,7 @@ if __name__ == "__main__":
 
     ftc_data = fetch_data(query_term=query, key_search_wrds=key_wrds_search, key_fetch_terms=key_wrds_fetch)
     db_dict = ftc_data.db_with_most_records_for_query()
-    ftc_data.fetch_data(db_dict=db_dict)
-    sort_retrieved_abstracts('all_abstracts', 'all_abstracts_pubdate')
+    file_name = ftc_data.fetch_data(db_dict=db_dict)
+    sort_retrieved_abstracts(file_name)
 
 
